96e4e8a1628beb1d40c1dcb84871e9e08f6d7120
k1pl1ng <81470564+k1pl1ng@users.noreply.github.com>
2021-03-27 15:57:08 +0300

    Add files via upload

---
+++ Tony Knnv/Tony Knnv/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace Tony_Knnv
+{
+    class Program
+    {
+        static void Main()
+        {
+            Registry.CurrentUser.CreateSubKey(@"Software\my project").SetValue("parameter", 1);
+        }
+    }
+}
